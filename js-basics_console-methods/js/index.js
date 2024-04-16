867317583d657135e9b2eb40c3b4e0a218806cb4
Sylvie Doumet <sylviedoumet@web.de>
2024-04-16 10:45:18 +0200

    update challenges

---
+++ js-basics_console-methods/js/index.js
@@ -0,0 +1,35 @@
+// The exercise starts here!
+
+// All console methods can be found on mdn for reference: https://developer.mozilla.org/en-US/docs/Web/API/console
+
+// 1. Store the sentence 'Happy coding!' in a variable named 'coding'.
+
+const coding = document.querySelector("h1");
+coding.textContent = "Happy coding!";
+
+// 2. Display the variable 'coding' as a message in the console.
+
+console.log("Happy coding!");
+
+// 3. Store the sentence 'Happy hacking!' in the variable named 'hacking'.
+
+const hacking = document.querySelector("h2");
+hacking.textContent = "Happy hacking!";
+
+// 4. Display the variable 'hacking' as a message in the console.
+
+console.log("Happy hacking!");
+
+// 5. Which method is used to clear the console? Use it to tidy up the console.
+
+console.clear();
+
+// 6. Store the text 'This is an error!' in the variable 'error'.
+
+const error = "This is an error!";
+
+// 7. Which method is used to display an error message in the console? Use it do display the error message in the console.
+
+console.assert("This is an error!");
+
+// Well done!
