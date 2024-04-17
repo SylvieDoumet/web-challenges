8b6bffac80bb7002c241d68dcdb32b874624c560
Sylvie Doumet <sylviedoumet@web.de>
2024-04-17 11:40:28 +0200

    update execises

---
+++ js-conditions-and-booleans_statements/index.js
@@ -0,0 +1,110 @@
+console.clear();
+
+// Part 1: Password
+const SUPER_SECRET_PASSWORD = "h4x0r1337";
+
+const receivedPassword = "password1234";
+
+const receivedPassword = false
+
+const passwordCorrect = SUPER_SECRET_PASSWORD;
+
+const statement = "Welcome! You are logged in as Brunhilde1984." + passwordCorrect;
+
+const statement = "Access denied!" + receivedPassword; 
+
+
+console.log (passwordCorrect);  
+
+
+
+
+// Part 2: Even / Odd
+const number = 6;
+
+const isEven = number;
+
+const isEven = true 
+
+const statement1 = "The number is even." + number;
+
+const statement2 = "The number is odd.";
+
+if (isEven) { }
+
+console.log (statement1)
+
+const (isEven) = false
+
+if (isEven) { } else { }
+
+console.log (statement2)
+
+
+
+// Part 3: Hotdogs
+
+// First part 
+
+const numberOfHotdogs = 42;
+
+const numberOfHotdogs = true; 
+
+if (numberOfHotdogs) { }    
+
+console.log ("You have to pay 1.50 Euro per hotdog.")
+
+// Second part
+
+if (numberOfHotdogs <5 ) { } else { }
+
+console.log ("You have to pay 2.00 Euro per hotdog.")
+
+if (numberOfHotdogs 5>100) { } else { }
+
+console.log ("You have to pay 1.50 Euro per hotdog.")
+
+if (numberOfHotdogs 100>1000000) { } else { }
+
+console.log ("You have to pay 1.00 Euro per hotdog.")
+
+if (numberOfHotdogs >1000000) { } else { }
+
+console.log ("You have to pay 0.10 Euro per hotdog.")
+
+
+// Part 4: Daytime
+const currentHour = 12;
+
+const statement1 = "Still need to learn...";
+
+const statement2 = "Partytime!!!"
+
+if (currentHour < 12) { } else { }  
+
+log.console (statement1);
+
+if (currentHour === 12) { } else { }
+
+log.console (statement2);
+
+if (currentHour > 12) { } else { }
+
+log.console (statement2);
+
+
+// Part 5: Greetings
+
+// Variante 1 ohne Ternary Operator 
+
+const userName = "Andrea";
+
+const greeting = 
+
+userName === "Andrea" ? console.log ("Hello Coach!") : console.log ("Hello " + userName + "!");
+
+console.log(greeting); 
+
+
+
+
---
+++ js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,32 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+const title = "My first post";
+const text = "This is my first post on social media!";
+const likes = 0;
+const user = "John Doe";
+const isReported = false;
+
+// --v-- write your code here --v--
+console.log(title);
+console.log(text);
+console.log(likes);
+console.log(user);
+console.log(isReported);
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+let newLikes = likes + 1;
+
+// --v-- write your code here --v--
+
+console.log(newLikes);
