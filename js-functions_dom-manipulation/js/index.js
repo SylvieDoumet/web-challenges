7d1f2f4891aa9fa80e91bf43029cba20cc560ef2
Sylvie Doumet <sylviedoumet@web.de>
2024-04-22 12:37:48 +0200

    solution js-functions-dom-manipulation

---
+++ js-functions_dom-manipulation/js/index.js
@@ -0,0 +1,51 @@
+const box = document.querySelector("[data-js=box]");
+const redButton = document.querySelector("[data-js=js-red-button]");
+const greenButton = document.querySelector("[data-js=js-green-button]");
+const blueButton = document.querySelector("[data-js=js-blue-button]");
+const grayButton = document.querySelector("[data-js=js-gray-button]");
+
+/*
+We want to add some functionality to the buttons on this page. When pressing a button, the corresponding color should be applied to the box.
+  1: Create a function removeAllColors which removes all color classes from the box.
+  2: Create a function addColor which takes a class name as a parameter and adds the class to the box.
+  3: Use the two functions in the event listeners of the buttons to color in the box when the button is clicked.
+*/
+
+function removeAllColors() {
+  // remove all classes from the box element
+  document.getElementById("button").style.backgroundColor = "white";
+}
+
+function addColor() {
+  newColor;
+  // add the class name to the box element
+}
+
+const newColor = document.getElementById("button").style.backgroundColor;
+
+// document.getElementById("button").bgcolor="#Insert Color Here";
+
+// document.getElementById("button").style.backgroundColor = "red";
+// document.getElementById("button").style.backgroundColor = "blue";
+// document.getElementById("button").style.backgroundColor = "green";
+// document.getElementById("button").style.backgroundColor = "gray";
+
+redButton.addEventListener("click", () => {
+  removeAllColors();
+  addColor("red");
+});
+
+blueButton.addEventListener("click", () => {
+  removeAllColors();
+  addColor("blue");
+});
+
+greenButton.addEventListener("click", () => {
+  removeAllColors();
+  addColor("green");
+});
+
+grayButton.addEventListener("click", () => {
+  removeAllColors();
+  addColor("gray");
+});
