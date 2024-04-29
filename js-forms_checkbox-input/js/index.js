e4573e48198e97bc786e48262566b847e0b9ba93
Sylvie Doumet <sylviedoumet@web.de>
2024-04-29 12:34:13 +0200

    last changes

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -3,10 +3,22 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
+
+function hideSuccess() {
+  successMessage.setAttribute("hidden", "");
+}
+
+function showSuccess() {       
+  successMessage.removeAttribute("hidden");
+}
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
 }
+
+hideSuccess();
+
 
 function showTosError() {
   tosError.removeAttribute("hidden");
@@ -27,4 +39,5 @@ form.addEventListener("submit", (event) => {
   }
 
   alert("Form submitted");
+  showSuccess();
 });
