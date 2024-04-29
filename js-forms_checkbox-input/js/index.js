9121cfcf96f9efa435215f7085654923cc6ccccc
Sylvie Doumet <sylviedoumet@web.de>
2024-04-29 11:00:53 +0200

    solution_js_forms_2_checkbox_input

---
+++ js-forms_checkbox-input/js/index.js
@@ -0,0 +1,26 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+const tosError = document.querySelector('[data-js="tos-error"]');
+const tosCheckbox = document.querySelector('[data-js="tos"]');
+
+function hideTosError() {
+  tosError.setAttribute("hidden", "");
+}
+
+function showTosError() {
+  tosError.removeAttribute("hidden");
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (tosCheckbox.checked === false) {
+    showTosError();
+    return;
+  }
+
+  hideTosError();
+
+  alert("Form submitted");
+});
