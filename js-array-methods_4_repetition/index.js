012c845afd4536f8bca00527218af65ac7ad43ef
Sylvie Doumet <sylviedoumet@web.de>
2024-05-08 08:46:24 +0200

    array_method_4_filter_map

---
+++ js-array-methods_4_repetition/index.js
@@ -0,0 +1,78 @@
+const recipes = [
+  {
+    title: "Crepes",
+    duration: 60,
+    ingredients: ["butter", "flour", "eggs", "milk", "salt"],
+    servings: 3,
+  },
+  {
+    title: "Scrambled Eggs",
+    duration: 20,
+    ingredients: ["eggs", "milk", "salt"],
+    servings: 2,
+  },
+  {
+    title: "Vegan Salmon",
+    duration: 60 * 24 * 3, // 3 days
+    ingredients: [
+      "carrots",
+      "olive oil",
+      "nori sheets",
+      "liquid smoke",
+      "soy sauce",
+    ],
+    servings: 10,
+  },
+  {
+    title: "Carrot Cake",
+    duration: 120,
+    ingredients: ["carrots", "flour", "eggs", "salt", "milk", "sugar"],
+    servings: 10,
+  },
+];
+
+/*
+ 1: `map` exercises
+*/
+
+const onlyTitles = cards.map((card) => card.title);
+
+const titlesWithDuration = cards.map((card) => card.title && card.title.number);
+
+const timePerServing = cards.map((card) => card.servings > 0);
+
+// EXTRA:
+
+// HINT: use first map() and then Array.prototype.join() with "method chaining" like so:
+// myArray.map(...).join(', ');
+
+const allTitlesInOneString = cards.map((card) => card.title).join(", ");
+
+// For more information how to join an array,
+// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join#joining_an_array_four_different_ways
+
+/*
+ 2: `filter` exercises
+*/
+
+const recipesThatOnlyTake60minutesOrLess = cards.filter(
+  (card) => card.duration < 60
+);
+
+const allRecipesWithMoreThan2Servings = cards.filter(
+  (card) => card.servings > 2
+);
+
+const allRecipesWithTitlesLongerThan12Characters = cards.filter(
+  (card) => card.title.length > 12
+);
+
+export {
+  onlyTitles,
+  titlesWithDuration,
+  timePerServing,
+  allTitlesInOneString,
+  recipesThatOnlyTake60minutesOrLess,
+  allRecipesWithMoreThan2Servings,
+  allRecipesWithTitlesLongerThan12Characters,
+};
