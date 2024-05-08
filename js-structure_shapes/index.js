ee4a2ca8de40035d6c4e9ae55d8c59421237fe28
Sylvie Doumet <sylviedoumet@web.de>
2024-05-08 12:47:20 +0200

    js_structure_1-shapes

---
+++ js-structure_shapes/index.js
@@ -0,0 +1,17 @@
+console.clear();
+
+import { Circle } from "./components/Circle/Circle.js";
+
+import { Pentagon } from "./components/Pentagon/Pentagon.js";
+
+import { Square } from "./components/Square/Square.js";
+
+const root = document.getElementById("root");
+
+const circle = Circle();
+
+const square = Square();
+
+const pentagon = Pentagon();
+
+root.append(circle, square, pentagon);
