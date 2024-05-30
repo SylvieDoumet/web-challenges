ad783b5ad6db81cb75efacc025138632eef018dc
Sylvie Doumet <sylviedoumet@web.de>
2024-05-30 11:55:09 +0200

    react2 - exercise2

---
+++ react-props_smiley/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+function Smiley({ isHappy }) {
+  return <h1>{isHappy ? "😀" : "😢"}</h1>;
+}
+
+export default function App() {
+  return <Smiley isHappy={true} />;
+}
