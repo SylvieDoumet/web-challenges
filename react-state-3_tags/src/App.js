8b252d5747cb418bd365abff952bde4481591bd8
Sylvie Doumet <sylviedoumet@web.de>
2024-06-07 12:12:18 +0200

    react-state3-exercise2

---
+++ react-state-3_tags/src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./styles.css";
+import Form from "./components/Form/index.js";
+import List from "./components/List/index.js";
+
+export default function App() {
+  const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+
+  // Function to add a new tag
+
+  function handleAddTag(newTag) {
+    setTags((prevTags) => [...prevTags, newTag]);
+  }
+
+  // Function to delete a tag
+
+  function handleDeleteTag(tagToDelete) {
+    setTags((prevTags) => prevTags.filter((tag) => tag !== tagToDelete));
+  }
+
+  return (
+    <main className="app">
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
+    </main>
+  );
+}
