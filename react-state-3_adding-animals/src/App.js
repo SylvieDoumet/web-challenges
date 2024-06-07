3f36aa97a167bb62948415f733ad7aaf546a1873
Sylvie Doumet <sylviedoumet@web.de>
2024-06-07 11:51:53 +0200

    react-states3-exercise1

---
+++ react-state-3_adding-animals/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./styles.css";
+import { uid } from "uid";
+import Form from "./components/Form/index.js";
+import List from "./components/List/index.js";
+
+const initialAnimals = [
+  {
+    id: "mTBErig",
+    name: "Octopus",
+    emoji: "🐙",
+  },
+  {
+    id: "iMsbXQ1",
+    name: "Crocodile",
+    emoji: "🐊",
+  },
+  {
+    id: "Qjf_K7V",
+    name: "Beaver",
+    emoji: "🦫",
+  },
+];
+
+export default function App() {
+  const [animals, setAnimals] = useState(initialAnimals);
+
+  function handleAddAnimal(newAnimal) {
+    const animalWithId = { ...newAnimal, id: uid() };
+    setAnimals((prevAnimals) => [...prevAnimals, animalWithId]);
+  }
+
+  return (
+    <main className="app">
+      <Form onAddAnimal={handleAddAnimal} />
+      <List animals={animals} />
+    </main>
+  );
+}
