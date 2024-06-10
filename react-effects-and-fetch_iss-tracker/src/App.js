3a38a4593d520f499e766d5c397c682460bc09c3
Sylvie Doumet <sylviedoumet@web.de>
2024-06-10 11:51:00 +0200

    react use&fetch - exercise3

---
+++ react-effects-and-fetch_iss-tracker/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Controls from "./components/Controls";
+import Map from "./components/Map";
+import "./styles.css";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+export default function App() {
+  const [coords, setCoords] = useState({
+    longitude: 0,
+    latitude: 0,
+  });
+
+  // Einfügen einer Funktion getISSCoords welche die ISS Koordinaten fetched und den state updatet
+
+  async function getISSCoords() {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setCoords({
+        longitude: data.longitude,
+        latitude: data.latitude,
+      });
+
+      // Einfügen einer Error handling Funktion - Error message
+    } catch (error) {
+      console.log("Error occured fetching coordinates:", error);
+    }
+  }
+
+  // Einfügen einer useEffect Funktion zum initialen Fetchen beim Mounting der Seite
+
+  useEffect(() => {
+    getISSCoords();
+
+    // Einfügen einer Intervall Funktion die alle 5 Sekunden einen fetch ausführt
+
+    const intervalId = setInterval(getISSCoords, 5000);
+  });
+
+  return (
+    <main>
+      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Controls
+        longitude={coords.longitude}
+        latitude={coords.latitude}
+        onRefresh={getISSCoords}
+      />
+    </main>
+  );
+}
