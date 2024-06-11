3eea0bf68cebc47539d0cb4e6270ac1b30dedc72
Sylvie Doumet <sylviedoumet@web.de>
2024-06-11 11:07:02 +0200

    storage - selfdirected learning-exercise3

---
+++ react-with-local-storage_dice/src/App.js
@@ -0,0 +1,46 @@
+
+// import von useLocalStorageState und .utils einfügen 
+
+import useLocalStorageState from 'use-local-storage-state'
+import { getD6Roll } from "./utlis";
+
+import { D6Button } from "./components/D6Button";
+import History from "./components/History";
+import "./styles.css";
+
+export default function App() {
+
+
+  // consts und setRoll einfügen 
+
+const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+
+const handleRoll = () => {
+
+  setRolls([{ VALUE: getD6Roll(), TIME: Date.now() }, ...rolls]);
+};
+
+const currentRollValue = rolls[0]?.value;
+
+
+
+  return (
+    <div className="app">
+      <main className="app__main">
+
+
+ {/* button adaptieren  */}
+
+        <D6Buttonvalue={currentRollValue} onRoll={handleRoll} />
+
+      </main>
+      <aside className="app__aside">
+
+{/* history anpassen {} -> {rolls} */}
+        <History rolls={rolls} />
+
+
+      </aside>
+    </div>
+  );
+}
