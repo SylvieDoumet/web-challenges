241276d3717717f57f73621c0ef54505ed235828
Sylvie Doumet <sylviedoumet@web.de>
2024-07-05 08:56:07 +0200

    react-data-fetching-star-wars_mission-completed

---
+++ react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,39 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+const fetcher = (url) =>
+  fetch(url).then((res) => {
+    if (!res.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+      error.info = res.statusText;
+      error.status = res.status;
+      throw error;
+    }
+    return res.json();
+  });
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, error } = useSWR(
+    id ? `https://swapi.dev/api/people/${id}/` : null,
+    fetcher
+  );
+
+  if (error) return <div>Failed to load character. Error: {error.message}</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
